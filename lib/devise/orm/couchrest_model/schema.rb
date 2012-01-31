aef98bceabc6936292a127884205cb4054ed18d3
Leon Du <leondu@gmail.com>
2012-01-31 15:51:23 +0800

    add "case_insensitive_keys" and "strip_whitespace_keys" patch

--- lib/devise/orm/couchrest_model/schema.rb
+++ lib/devise/orm/couchrest_model/schema.rb
@@ -10,6 +10,9 @@ module Devise
         end
 
         def find_for_authentication(conditions)
+          conditions = filter_auth_params(conditions.dup)
+          (case_insensitive_keys || []).each { |k| conditions[k].try(:downcase!) }
+          (strip_whitespace_keys || []).each { |k| conditions[k].try(:strip!) }
           find(:conditions => conditions)
         end
 
@@ -24,7 +27,21 @@ module Devise
             find_by_key_and_value(:id, id)
           end
         end
+
+        protected
+
+        # Force keys to be string to avoid injection on mongoid related database.
+        def filter_auth_params(conditions)
+          conditions.each do |k, v|
+            conditions[k] = v.to_s if auth_param_requires_string_conversion?(v)
+          end if conditions.is_a?(Hash)
+        end
         
+        # Determine which values should be transformed to string or passed as-is to the query builder underneath
+        def auth_param_requires_string_conversion?(value)
+          true unless value.is_a?(TrueClass) || value.is_a?(FalseClass) || value.is_a?(Fixnum)
+        end
+
         private
         
         def find_by_key_and_value(key, value)
